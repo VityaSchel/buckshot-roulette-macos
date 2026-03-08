28009af8b9c6d87472d343b9ac0c89268631e2ec
Viktor Shchelochkov <hi@hloth.dev>
2026-03-08 19:24:01 +0100

    Replace backend /download endpoint logic with native Bun's handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,59 +8,18 @@ const appProtected = new Elysia({
 	.use(appAuth)
 	.get(
 		'/download',
-		async ({ cookie: { license }, redirect, set, headers }) => {
+		async ({ cookie: { license }, redirect }) => {
 			const token = license!.value;
 			if (!token || token.expiresAt <= Date.now()) {
 				return redirect(new URL('auth', PUBLIC_API_URL).href);
 			}
 
-			let file = Bun.file(
+			return Bun.file(
 				new URL(
 					'../../static/buckshot-roulette-macos-v1.2.zip',
 					import.meta.url,
 				),
 			);
-			if (!(await file.exists())) {
-				set.status = 500;
-				return { ok: false, error: 'Try again later' };
-			}
-
-			const range = headers['range'];
-			let start = 0;
-			let end = file.size - 1;
-
-			if (range) {
-				const rangeParts = range.replace(/bytes=/, '').split('-');
-				const rangeStart = rangeParts[0];
-				if (!rangeStart) {
-					set.status = 400;
-					return { ok: false, error: "Invalid 'Range' header" };
-				}
-				start = parseInt(rangeStart, 10);
-				end = rangeParts[1] ? parseInt(rangeParts[1], 10) : end;
-
-				if (start >= file.size || end >= file.size) {
-					set.headers['Content-Range'] = `bytes */${file.size}`;
-					set.status = 416;
-					return { ok: false, error: 'Requested Range Not Satisfiable' };
-				}
-
-				set.headers['Content-Range'] = `bytes ${start}-${end}/${file.size}`;
-				set.headers['Content-Length'] = String(end - start + 1);
-			} else {
-				set.headers['Content-Length'] = String(file.size);
-			}
-
-			// set.headers['Accept-Ranges'] = 'bytes'
-			set.headers['Content-Type'] = 'application/octet-stream';
-			set.headers['Content-Disposition'] =
-				`attachment; filename="buckshot-roulette-macos-v1.2.zip"`;
-
-			if (range) {
-				file = file.slice(start, end + 1);
-			}
-
-			return file.stream();
 		},
 		{
 			cookie: authCookieSchema,
